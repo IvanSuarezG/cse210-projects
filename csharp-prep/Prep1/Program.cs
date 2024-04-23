0f142008d329c4a7420c472e782cc0773ed3be87
IvanSuarezG <sua23009@byui.edu>
2024-04-23 16:09:00 -0500

    adjustments

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Diagnostics;
 
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+    }
+
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
     }
 }
