39311ac65d4bc1e0cc7e044f8c0e3596b315b7c8
Ivan Suarez <sua230009@byui.edu>
2024-05-06 03:51:01 -0500

    exercise of list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,50 @@
 using System;
 
+using System.Collections.Generic;
+using System.Globalization;
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int num = 1;
+        int sum = 0;
+        double average = 9;
+        int bigger = 0;
+
+
+        while (num != 0){
+            Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+            string num_entered = Console.ReadLine();
+            num = int.Parse(num_entered);
+
+            if (num != 0)
+            {
+                numbers.Add(num);
+            }  
+        }
+        foreach (int number in numbers){
+            sum+= number;
+            
+        }
+        Console.WriteLine($"The sum is: {sum}");
+        int amount = numbers.Count;
+
+        average = sum / amount;
+        Console.WriteLine($"The average is: {average}");
+
+        foreach (int number in numbers){
+            if(number > bigger){
+                    bigger = number;
+                }
+        }
+        Console.WriteLine($"The largest number is: {bigger}");
+        
+        
+
     }
 }
+
+
+    
