2ae5bf1138aa27f73a6fb9a41cf51a19cf774ddf
Ivan Suarez <sua230009@byui.edu>
2024-05-06 02:19:44 -0500

    magic number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Start:
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,100);
+        
+        int attemps = 0;
+        string try_again = "";
+
+        while (try_again != "no")
+        {
+            Console.WriteLine("What is the magic number?");
+            string temp2 = Console.ReadLine();
+            int guess_number = int.Parse(temp2);
+
+            if (guess_number > number)
+            {
+                Console.WriteLine("Higher");
+                attemps = attemps + 1;
+            }else if (guess_number < number)
+            {
+                 Console.WriteLine("Lower");
+                 attemps = attemps + 1;
+            }else{
+                 attemps = attemps + 1;
+                 Console.WriteLine("Congratulations!");
+                 Console.WriteLine("You guess it !");
+                 Console.WriteLine($"Intents: {attemps}");
+                 Console.WriteLine("Do you want to play again?");
+                 try_again = Console.ReadLine();
+                 if (try_again == "yes"){
+                    attemps = 0;
+                    goto Start;
+                 }
+            }
+        }
+        Console.WriteLine("End Program!");
     }
 }
