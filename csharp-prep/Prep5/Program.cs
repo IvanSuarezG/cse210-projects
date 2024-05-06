e5fd3e22be44d83357fed6a698744667d20f6b48
Ivan Suarez <sua230009@byui.edu>
2024-05-06 04:16:07 -0500

    Functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int couple = SquareNumber(number);
+        DisplayResult(couple, name);
+        
+    }
+
+    static void DisplayWelcome(){
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName(){
+        Console.Write("What is your name?  ");
+        string _name = Console.ReadLine();
+
+        return _name;
+    }
+
+    static int PromptUserNumber(){
+        Console.Write("Please enter your favorite number: ");
+        string num = Console.ReadLine();
+        int number = int.Parse(num);
+        return number;
+    }
+
+    static int SquareNumber(int value){
+           int square = value * value;
+           return square;
+    }
+
+    static void DisplayResult(int amount, string date){
+        DisplayWelcome();
+        Console.WriteLine($"{date}, The square of your number is {amount}");
+    
     }
 }
