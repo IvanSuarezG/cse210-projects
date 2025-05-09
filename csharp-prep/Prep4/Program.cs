b8ac6e2077101aec18eccd6abfe560f00a18823f
Ivan Suarez <sua230009@byui.edu>
2025-05-09 02:26:30 -0500

    list Exercise

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -11,11 +11,10 @@ class Program
         int num = 1;
         int sum = 0;
         double average = 9;
-        int bigger = 0;
+        int bigger = int.MinValue;
 
-
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
         while (num != 0){
-            Console.WriteLine("Enter a list of numbers, type 0 when finished.");
             string num_entered = Console.ReadLine();
             num = int.Parse(num_entered);
 
@@ -24,25 +23,23 @@ class Program
                 numbers.Add(num);
             }  
         }
-        foreach (int number in numbers){
-            sum+= number;
-            
-        }
-        Console.WriteLine($"The sum is: {sum}");
-        int amount = numbers.Count;
-
-        average = sum / amount;
-        Console.WriteLine($"The average is: {average}");
-
-        foreach (int number in numbers){
+        foreach (int number in numbers)
+        {
+            sum += number;
             if(number > bigger){
                     bigger = number;
                 }
+            
         }
-        Console.WriteLine($"The largest number is: {bigger}");
         
-        
-
+        int amount = numbers.Count;
+        if (amount > 0)
+        {
+            average = sum / amount;
+            Console.WriteLine($"The sum is: {sum}");
+            Console.WriteLine($"The average is: {average}");
+            Console.WriteLine($"The largest number is: {bigger}");
+        }
     }
 }
 
